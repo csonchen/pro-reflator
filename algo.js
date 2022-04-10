245489e2d7a67b0b7526ebea7afa22a0bbaa278e
semchen <semchen@tencent.com>
2022-04-10 21:19:39 +0800

    feat: add graph algo`

--- algo.js
+++ algo.js
@@ -34,7 +34,93 @@ function sum(arr) {
   }
 }
 
+function count(arr) {
+  if (arr.length === 0) {
+    return 0;
+  } else {
+    // delete one num
+    arr.splice(0, 1);
+    return 1 + count(arr);
+  }
+}
+
+function maxNum(arr) {
+  if (arr.length < 2) {
+    throw new Error('your array size must more than 2.')
+  }
+  if (arr.length === 2) {
+    return arr[0] > arr[1] ? arr[0] : arr[1]
+  } else {
+    let lastNum = arr[0];
+    // delete last num
+    arr.splice(0, 1);
+    let subMax = maxNum(arr);
+    return lastNum > subMax ? lastNum : subMax;
+  }
+}
+
+const processed = [];
+
+function findLowestNode(costs) {
+  let lowestCost = Infinity;
+  let lowestCostNode = null;
+  for (const property in costs) {
+    let cost = costs[property];
+    if (cost < lowestCost && !processed.includes(property)) {
+      lowestCost = cost;
+      lowestCostNode = property;
+    }
+  }
+  return lowestCostNode;
+}
+
+function weightedGraph(graph, costs, parents) {
+  let node = findLowestNode(costs);
+  while (node !== null) {
+    cost = costs[node];
+    neighbors = graph[node];
+
+    for (let key of Object.keys(neighbors)) {
+      let newCost = cost + neighbors[key];
+      if (costs[key] > newCost) {
+        costs[key] = newCost;
+        parents[key] = key;
+      }
+    }
+    processed.push(node);
+    node = findLowestNode(costs);
+  }
+}
+
+const searched = [];
+
+function isMatcher(str) {
+  return str[str.length - 1] === 'm';
+}
+
+function depthInSearch(graph, key) {
+  let queue = graph[key];
+
+  while (queue.length) {
+    const person = queue.shift();
+    if (!searched.includes(person)) {
+      if (isMatcher(person)) {
+        return person;
+      } else {
+        searched.push(person);
+        queue = [...queue, ...graph[person]];
+      }
+    }
+  }
+  return null;
+}
+
 module.exports = {
   quickSort,
   sum,
+  count,
+  maxNum,
+  findLowestNode,
+  weightedGraph,
+  depthInSearch,
 }
--- test/algo.test.js
+++ test/algo.test.js
@@ -1,5 +1,12 @@
 const expect = require('chai').expect;
-const { quickSort, sum } = require('../algo');
+const { 
+  quickSort, 
+  sum, 
+  count, 
+  maxNum, 
+  weightedGraph, 
+  depthInSearch,
+} = require('../algo');
 
 describe('quickSort', () => {
   it('normal array', () => {
@@ -19,4 +26,66 @@ describe('sum', () => {
   it('empty array', () => {
     expect(sum([])).equal(0);
   })
-})
+});
+
+describe('count', () => {
+  it('normal array', () => {
+    expect(count([2, 2, 34, 323])).equal(4);
+  });
+});
+
+describe('maxNum', () => {
+  it('normal array', () => {
+    expect(maxNum([1,2, 23,12, 199, 1000])).equal(1000);
+  })
+});
+
+describe('weighted graph', () => {
+  it('normal', () => {
+    // 每个节点到兄弟节点花销
+    const graph = {
+      start: {
+        a: 6,
+        b: 2,
+      },
+      a: {
+        fin: 1,
+      },
+      b: {
+        a: 3,
+        fin: 5
+      },
+      fin: {}
+    };
+    // 当前花销集合
+    const costs = {
+      a: 6,
+      b: 2,
+      fin: Infinity,
+    };
+    // 父节点
+    const parents = {
+      a: 'start',
+      b: 'start',
+      fin: null
+    };
+    weightedGraph(graph, costs, parents);
+    expect(costs['fin']).equal(6);
+  })
+});
+
+describe('depthInSearch', () => {
+  it('normal', () => {
+    const graph = {
+      you: ['alice', 'bob', 'clarire'],
+      bob: ['anuj', 'peggy'],
+      alice: ['peggy'],
+      clarire: ['thom', 'jonny'],
+      anuj: [],
+      peggy: [],
+      thom: [],
+      jonny: [],
+    }
+    expect(depthInSearch(graph, 'you')).equal('thom');
+  });
+});
